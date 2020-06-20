ddac033cd826a769866436bd68b90f58c0db707a
danite <luansnain@gmail.com>
2020-06-20 17:24:08 +0200

    Implement custom error handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express, { Response, Request } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email is required"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new Error("Invalid password or email");
+    }
+
+    console.log("Creating user");
+
+    throw new Error("Error connecting to database");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };

07443bfc6e0177c8aba1d35b3611dce16b4280e6
danite <luansnain@gmail.com>
2020-09-25 12:34:59 +0200

    Finish testing singup route

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -12,3 +12,69 @@ it("returns 201 on successful sign up", async () => {
     })
     .expect(201);
 });
+
+it("returns 400 with invalid email", async () => {
+  return request(app)
+    .post(singupUrl)
+    .send({
+      email: "testcom",
+      password: "test",
+    })
+    .expect(400);
+});
+
+it("returns 400 with invalid password", async () => {
+  return request(app)
+    .post(singupUrl)
+    .send({
+      email: "test@test.com",
+      password: "t",
+    })
+    .expect(400);
+});
+
+it("returns 400 with missing email and password", async () => {
+  await request(app)
+    .post(singupUrl)
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+
+  return request(app)
+    .post(singupUrl)
+    .send({
+      password: "test",
+    })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post(singupUrl)
+    .send({
+      email: "test@test.com",
+      password: "test",
+    })
+    .expect(201);
+
+  await request(app)
+    .post(singupUrl)
+    .send({
+      email: "test@test.com",
+      password: "test",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after a successful signup", async () => {
+  const response = await request(app)
+    .post(singupUrl)
+    .send({
+      email: "test@test.com",
+      password: "test",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
